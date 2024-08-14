bb4f2a2b6729511eb7f3fc88bbfe45fc193b914d
Ryan Baker <rbaker@xtier.com>
2024-08-14 17:32:41 -0400

    Disabled auto deletion of db

--- src/website/models.py
+++ src/website/models.py
@@ -3,7 +3,6 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     __tablename__ = "user"
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, name="email")
     password = db.Column(db.String(150))
