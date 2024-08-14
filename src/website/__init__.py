f161b96b6f091887309f776283742bcbcc649cbf
Ryan Baker <rbaker@xtier.com>
2024-08-14 14:37:36 -0400

    Added website import __init__.py, will need to configure database before fully implementing application

---
+++ src/website/__init__.py
@@ -0,0 +1,27 @@
+from flash import Flask
+from flask_login import LoginManager
+
+# declare database
+
+def createApp():
+        app = Flask(__name__)
+        app.config["SECRET_KEY"] = "testKey"
+        
+        # configure database
+        # init database
+
+        from .views import views
+        from .auth import auth
+        
+        app.register_blueprint(views, url_prefix='/')
+        app.register_blueprint(auth, url_prefix='/')
+
+        # register database models
+
+        login_manager = LoginManager()
+        login_manager.login_view = "views.home"
+        login_manager.init_app(app)
+
+        # load user function
+
+        return app
