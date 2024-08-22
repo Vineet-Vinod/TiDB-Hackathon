ff57574804e5e8a0c41a51be7b7895d125487b20
Vineet Vinod <vineetv314@gmail.com>
2024-08-21 21:11:40 -0400

    Update template movies

--- src/website/scripts.py
+++ src/website/scripts.py
@@ -1,10 +1,12 @@
 def preferenceTuningPosters():
     # will need to implement a more sophisticated algorithm here
     return [
-        "https://images.photowall.com/products/51078/movie-poster-jaws.jpg?h=699&q=85",
-        "https://filmartgallery.com/cdn/shop/products/Shrek-Vintage-Movie-Poster-Original-1-Sheet-27x41.jpg?v=1665732097",
-        "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
-        "https://i.etsystatic.com/18242346/r/il/7ad080/4580913585/il_fullxfull.4580913585_3yhu.jpg",
-        "https://intheposter.com/cdn/shop/products/the-front-line-in-the-poster-1_1600x.jpg?v=1694762475",
+        "https://imgs.search.brave.com/fw3zN0uOwFKoIAcsjUK27eyBalIZfrzu55vnQ-pYgqg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzgxYUhldUowQ0dM/LmpwZw",
+        "https://imgs.search.brave.com/bY-g_9l2aRiMqh5v4U8ZNOXSjyLS50eT6Sqc9qp9kA4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9oaXBz/LmhlYXJzdGFwcHMu/Y29tL2htZy1wcm9k/L2ltYWdlcy90aXRh/bmljLWZhY3RzLW1v/dmllLXBvc3Rlci0x/NTEyNjY0MjQ2Lmpw/Zz9jcm9wPTAuOTQ1/eHc6MC45NTV4aDsw/LjAyODd4dywwLjAx/Njd4aCZyZXNpemU9/OTgwOio",
+        "https://imgs.search.brave.com/fsfXFyz3O3g5cYrfd-pvPw-Eyh9eoFMxVqMELv579CY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1Qk1qSXdNakUx/TnpjNE5WNUJNbDVC/YW5CblhrRnRaVGd3/TkRnNE9UQTFOek1A/LmpwZw",
+        "https://imgs.search.brave.com/pbA7p0uoB5HP1uEe2EOwTAOsNE-Coe6-8xqcbhOWBA8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1Qk0yTXlOall4/Tm1VdFlUQXdOaTAw/TVRZeExXSm1OV1l0/WXpabE9EWTNaVGsz/T1RGbFhrRXlYa0Zx/Y0dkZVFYVnlOemt3/TWpRNU56TUAuanBn",
+        "https://imgs.search.brave.com/3vNTQPgREOv9oYhvDjVlOioq9F5UmiB16GdAOhar-Qc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QlpETXlaR0pq/T0dJdFlqSmtaQzAw/TURWbExXRTBZMll0/WkdJd01ERXhZV0Uz/TUdRM1hrRXlYa0Zx/Y0dkZVFYVnlORFl5/TURrNU1UVUAuanBn",
         "https://cdn.prod.website-files.com/6009ec8cda7f305645c9d91b/66a4263d01a185d5ea22eeef_6408f676b5811234c887ca62_top%2520gun%2520maverick-min.png",
+        "https://imgs.search.brave.com/QX0RXW6vr2GwWZx46WqBQSh9nPqv4evZH6ZDLk4TMQQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGVw/aGVua2luZy5jb20v/aW1hZ2VzL21vdmll/cy90aGVhdHJpY2Fs/X21vdmllL3RoZV9z/aGF3c2hhbmtfcmVk/ZW1wdGlvbi91czIu/anBn",
+        "https://imgs.search.brave.com/w4s-Y69uwePCFWHB0qKu1N79ujllOIGN5R3-3t8QDG8/rs:fit:860:0:0:0/g:ce/aHR0cDovL3d3dy5p/bXBhd2FyZHMuY29t/LzIwMDgvcG9zdGVy/cy9kYXJrX2tuaWdo/dC5qcGc",
     ]
