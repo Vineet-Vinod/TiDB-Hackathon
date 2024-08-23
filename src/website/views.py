44943a7c71123749f74c266bf0a9d8a4313c72ec
Ryan Baker <rbaker@xtier.com>
2024-08-23 10:36:45 -0400

    Will fix fornt end bug w/ movie displays

--- src/website/views.py
+++ src/website/views.py
@@ -68,6 +68,4 @@ def tune_preferences(): # They should get this page only when they create an acc
 def get_recommendations():
     if request.method == "POST":
         query = request.form["prompt"]
-        result = db.get_recommendations(query, session["email"])
-
-    return render_template("home.html", messages=result)
+        results = db.get_recommendations(query, session["email"])
